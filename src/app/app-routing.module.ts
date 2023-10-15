6c941b820c9775cfa2bcdf2c7e08985a5132097f
Emma Mora <emmanu20021@gmail.com>
2023-10-14 22:35:21 -0600

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch : 'full', redirectTo: 'home'},
+  {
+    path: '',
+    component: HomeComponent,
+    children : [
+      { path: 'home', loadChildren: () => import("src/app/modules/home/home-routing.module").then(m => m.HomeRoutingModule)},
+    ]
+  },
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
